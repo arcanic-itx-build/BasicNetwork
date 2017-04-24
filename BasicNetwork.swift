7058403a627f06efec2d541cb6b0335d302244bc
Theis Egeberg <theis.egebrerg@gmail.com>
2017-04-24 13:58:46 +0200

    Moved to using data instead of JSON

--- BasicNetwork.swift
+++ BasicNetwork.swift
@@ -12,7 +12,7 @@ import Foundation
 
 public class BasicNetwork {
     
-    public static let defaultCompletionHandler:JSONCompletionHandler = { (response) in
+    public static let defaultCompletionHandler:CompletionHandler = { (response) in
         switch response {
         case .error(let error, let report):
             print("ERROR")
@@ -40,22 +40,7 @@ public class BasicNetwork {
     }
     
     public enum NetworkMode {
-        case localhost,development,production,placeholder
-        
-        var server:String {
-            get {
-                switch self {
-                case .localhost:
-                    return "http://0.0.0.0:8080"
-                case .development:
-                    return "http://theisegeberg.com:8080"
-                case .production:
-                    return "http://theisegeberg.com:8081"
-                case .placeholder:
-                    return "https://jsonplaceholder.typicode.com"
-                }
-            }
-        }
+        case localhost,development,production
     }
     
     
@@ -76,15 +61,14 @@ public class BasicNetwork {
     
     public enum BasicNetworkError:Error {
         case httpError(statusCode:Int,description:String)
-        case couldNotFormatFormatURL(String)
-        case couldNotSerializeJSON(String)
-        case noData
+        case urlCreationError(String)
+        case jsonSerializationError(String)
+        case dataMissingError
     }
     
-    
-    public enum JSONResponse {
+    public enum Response {
         case error(Error,report:RequestReport)
-        case success([String:Any],report:RequestReport)
+        case success(Data,report:RequestReport)
     }
     
     public struct RequestReport {
@@ -111,18 +95,19 @@ public class BasicNetwork {
     }
     
     
-    public typealias JSONCompletionHandler = (JSONResponse) -> ()
+    public typealias CompletionHandler = (Response) -> ()
     
-    public var mode:NetworkMode = .placeholder
+    public var server:String = "http://0.0.0.0:8080"
+    public var mode:NetworkMode = .localhost
     public var timeOut:TimeInterval = 5
     public var cachePolicy:URLRequest.CachePolicy = .reloadRevalidatingCacheData
     
-    public func request(endPoint:EndPoint,parameters:[String:Any]?,method:HTTPMethod,completionHandler:JSONCompletionHandler? = nil) {
+    public func request(endPoint:EndPoint,parameters:[String:Any]?,method:HTTPMethod,completionHandler:CompletionHandler? = nil) {
         
         var report = RequestReport()
         
-        guard let url = URL(string:"\(self.mode.server)/\(endPoint.description)") else {
-            completionHandler?(JSONResponse.error(BasicNetworkError.couldNotFormatFormatURL("\(self.mode.server)\(endPoint)"),report: report))
+        guard let url = URL(string:"\(self.server)/\(endPoint.description)") else {
+            completionHandler?(Response.error(BasicNetworkError.urlCreationError("\(self.server)\(endPoint)"),report: report))
             return
         }
         
@@ -144,7 +129,7 @@ public class BasicNetwork {
                 report.requestBody = String(data: requestBody, encoding: .utf8)
                 
             } catch let error {
-                completionHandler?(JSONResponse.error(error,report: report))
+                completionHandler?(Response.error(error,report: report))
             }
         }
         
@@ -157,12 +142,12 @@ public class BasicNetwork {
             report.requestHeaders = request.allHTTPHeaderFields
             
             guard error == nil else {
-                completionHandler?(JSONResponse.error(error!,report: report))
+                completionHandler?(Response.error(error!,report: report))
                 return
             }
             
             guard let data = data else {
-                completionHandler?(JSONResponse.error(BasicNetworkError.noData,report: report))
+                completionHandler?(Response.error(BasicNetworkError.dataMissingError,report: report))
                 return
             }
             
@@ -174,30 +159,19 @@ public class BasicNetwork {
                 report.responseHeaders = httpResponse.allHeaderFields
                 
                 if (httpResponse.statusCode >= 400) {
-                    completionHandler?(JSONResponse.error(BasicNetworkError.httpError(statusCode: httpResponse.statusCode, description: HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode)),report: report))
+                    completionHandler?(Response.error(BasicNetworkError.httpError(statusCode: httpResponse.statusCode, description: HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode)),report: report))
                     return
                 }
             }
             
             
-            
-            do {
-                
-                
-                guard let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String:Any] else {
-                    
-                    let rawResponse = String(data: data, encoding: .utf8) ?? "Warning: couldn't decode response from data to utf8"
-                    completionHandler?(JSONResponse.error(BasicNetworkError.couldNotSerializeJSON(rawResponse),report: report))
-                    return
-                }
-                
-                
-                
-                completionHandler?(JSONResponse.success(json,report: report))
-                
-            } catch let error {
-                completionHandler?(JSONResponse.error(error,report: report))
+            if let rawResponse = String(data: data, encoding: .utf8) {
+                report.responseBody = rawResponse
             }
+            
+            
+            completionHandler?(Response.success(json,report: report))
+            
         }
         task.resume()
     
---
+++ BasicNetwork/BasicNetwork.h
@@ -0,0 +1,19 @@
+//
+//  BasicNetwork.h
+//  BasicNetwork
+//
+//  Created by Theis Egeberg on 24/04/2017.
+//  Copyright © 2017 Theis Egeberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BasicNetwork.
+FOUNDATION_EXPORT double BasicNetworkVersionNumber;
+
+//! Project version string for BasicNetwork.
+FOUNDATION_EXPORT const unsigned char BasicNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicNetwork/PublicHeader.h>
+
+
